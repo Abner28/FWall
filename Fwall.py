d237f77d2a7366e03f0eafe0e992ced76798168f
Abner28 <65782683+Abner28@users.noreply.github.com>
2020-09-07 09:00:12 -0300

    Add files via upload

--- Fwall.py
+++ Fwall.py
@@ -294,8 +294,309 @@ class comparison_wing(QtWidgets.QMainWindow):
     """docstring for comparison_wing"""
     def __init__(self):
         super(comparison_wing, self).__init__()
-        self.resize(720, 540)
-        self.setWindowTitle("Comparison Window")   
+        self.resize(720, 680)
+        self.setWindowTitle("Comparison Window") 
+
+        # Botão:
+
+        self.run3Button = QPushButton("Compare",self)
+        self.run3Button.move(330,620)
+        self.run3Button.resize(100,50)
+
+
+        # CASE 1:
+
+        self.label_1_1 = QtWidgets.QLabel(self)
+        self.label_1_2 = QtWidgets.QLabel(self)
+        self.label_1_3 = QtWidgets.QLabel(self)
+        self.label_1_4 = QtWidgets.QLabel(self)
+        self.label_1_5 = QtWidgets.QLabel(self)
+        self.label_1_6 = QtWidgets.QLabel(self)
+        self.label_1_7 = QtWidgets.QLabel(self)
+        self.label_1_8 = QtWidgets.QLabel(self)
+        self.label_1_9 = QtWidgets.QLabel(self)
+        self.label_1_10 = QtWidgets.QLabel(self)
+        self.label_1_11 = QtWidgets.QLabel(self)
+        self.label_1_12 = QtWidgets.QLabel(self)
+        self.label_1_13 = QtWidgets.QLabel(self)
+
+        self.label_1_1.setText("CASE 1")
+        self.label_1_1.move(150,30)
+        self.label_1_1.setFont(QtGui.QFont("times",18))
+        self.label_1_1.adjustSize()
+
+        self.label_1_2.setText("Number of wings:")
+        self.label_1_2.move(50,105)
+        self.label_1_2.setFont(QtGui.QFont("Arial",12))
+        self.label_1_2.adjustSize()
+
+        self.textbox1_2 = QLineEdit(self)
+        self.textbox1_2.setFont(QtGui.QFont("times",14))
+        self.textbox1_2.move(200,100)
+           
+        self.label_1_3.setText("Wing span [m]:")
+        self.label_1_3.move(50,155)
+        self.label_1_3.setFont(QtGui.QFont("Arial",12))
+        self.label_1_3.adjustSize()
+
+        self.textbox_1_3 = QLineEdit(self)
+        self.textbox_1_3.move(200,150)
+        self.textbox_1_3.setFont(QtGui.QFont("times",14))
+        
+
+        self.label_1_4.setText("AoA [°]:")
+        self.label_1_4.move(50,210)
+        self.label_1_4.setFont(QtGui.QFont("Arial",12))
+        self.label_1_4.adjustSize()
+
+        self.textbox_1_4 = QLineEdit(self)
+        self.textbox_1_4.move(200,205)
+        self.textbox_1_4.setFont(QtGui.QFont("times",14))
+        
+
+        self.label_1_5.setText("y-offset [m]:")
+        self.label_1_5.move(50,270)
+        self.label_1_5.setFont(QtGui.QFont("Arial",12))
+        self.label_1_5.adjustSize()
+
+        self.textbox_1_5 = QLineEdit(self)
+        self.textbox_1_5.move(200,265)
+        self.textbox_1_5.setFont(QtGui.QFont("times",14))
+        
+
+        self.label_1_6.setText("z-offset [m]:")
+        self.label_1_6.move(50,325)
+        self.label_1_6.setFont(QtGui.QFont("Arial",12))
+        self.label_1_6.adjustSize()
+
+        self.textbox_1_6 = QLineEdit(self)
+        self.textbox_1_6.move(200,320)
+        self.textbox_1_6.setFont(QtGui.QFont("times",14))
+        
+
+        self.label_1_7.setText("Element type:")
+        self.label_1_7.move(50,375)
+        self.label_1_7.setFont(QtGui.QFont("Arial",12))
+        self.label_1_7.adjustSize()
+
+        self.textbox_1_7 = QLineEdit(self)
+        self.textbox_1_7.move(200,370)
+        self.textbox_1_7.setFont(QtGui.QFont("times",14))
+        
+        
+        self.label_1_8.setText("Mesh type:")
+        self.label_1_8.move(50,435)
+        self.label_1_8.setFont(QtGui.QFont("Arial",12))
+        self.label_1_8.adjustSize()
+
+        self.textbox_1_8 = QLineEdit(self)
+        self.textbox_1_8.move(200,430)
+        self.textbox_1_8.setFont(QtGui.QFont("times",14))
+        
+        self.label_1_10.setText("Wing type\n(Planar or Flexibe):")
+        self.label_1_10.move(50,475)
+        self.label_1_10.setFont(QtGui.QFont("Arial",12))
+        self.label_1_10.adjustSize()
+
+        self.textbox_1_10 = QLineEdit(self)
+        self.textbox_1_10.move(200,485)
+        self.textbox_1_10.setFont(QtGui.QFont("times",14))
+        
+
+        self.label_1_11.setText("Number of elements:")
+        self.label_1_11.move(40,535)
+        self.label_1_11.setFont(QtGui.QFont("Arial",12))
+        self.label_1_11.adjustSize()
+
+        self.textbox_1_11 = QLineEdit(self)
+        self.textbox_1_11.move(200,530)
+        self.textbox_1_11.setFont(QtGui.QFont("times",14))
+        
+
+        self.label_1_12.setText("r:")
+        self.label_1_12.move(170,580)
+        self.label_1_12.setFont(QtGui.QFont("Arial",12))
+        self.label_1_12.adjustSize()
+
+        self.textbox_1_12 = QLineEdit(self)
+        self.textbox_1_12.move(200,580)
+        self.textbox_1_12.setFont(QtGui.QFont("times",14))
+        
+     
+
+        self.label_1_13.setText("Uinf [m/s]:")
+        self.label_1_13.move(115,630)
+        self.label_1_13.setFont(QtGui.QFont("Arial", 12))
+
+        self.textbox_1_13 = QLineEdit(self)
+        self.textbox_1_13.move(200,630)
+        self.textbox_1_13.setFont(QtGui.QFont("times",14))
+
+
+        # CASE 2:
+
+        self.label_2_1 = QtWidgets.QLabel(self)
+        self.label_2_2 = QtWidgets.QLabel(self)
+        self.label_2_3 = QtWidgets.QLabel(self)
+        self.label_2_4 = QtWidgets.QLabel(self)
+        self.label_2_5 = QtWidgets.QLabel(self)
+        self.label_2_6 = QtWidgets.QLabel(self)
+        self.label_2_7 = QtWidgets.QLabel(self)
+        self.label_2_8 = QtWidgets.QLabel(self)
+        self.label_2_9 = QtWidgets.QLabel(self)
+        self.label_2_10 = QtWidgets.QLabel(self)
+        self.label_2_11 = QtWidgets.QLabel(self)
+        self.label_2_12 = QtWidgets.QLabel(self)
+        self.label_2_13 = QtWidgets.QLabel(self)
+
+        self.label_2_1.setText("CASE 2")
+        self.label_2_1.move(500,30)
+        self.label_2_1.setFont(QtGui.QFont("times",18))
+        self.label_2_1.adjustSize()
+
+        self.label_2_2.setText("Number of wings:")
+        self.label_2_2.move(400,105)
+        self.label_2_2.setFont(QtGui.QFont("Arial",12))
+        self.label_2_2.adjustSize()
+
+        self.textbox_2_2 = QLineEdit(self)
+        self.textbox_2_2.setFont(QtGui.QFont("times",14))
+        self.textbox_2_2.move(550,100)
+           
+        self.label_2_3.setText("Wing span [m]:")
+        self.label_2_3.move(400,155)
+        self.label_2_3.setFont(QtGui.QFont("Arial",12))
+        self.label_2_3.adjustSize()
+
+        self.textbox_2_3 = QLineEdit(self)
+        self.textbox_2_3.move(550,150)
+        self.textbox_2_3.setFont(QtGui.QFont("times",14))
+        
+
+        self.label_2_4.setText("AoA [°]:")
+        self.label_2_4.move(400,210)
+        self.label_2_4.setFont(QtGui.QFont("Arial",12))
+        self.label_2_4.adjustSize()
+
+        self.textbox_2_4 = QLineEdit(self)
+        self.textbox_2_4.move(550,205)
+        self.textbox_2_4.setFont(QtGui.QFont("times",14))
+        
+
+        self.label_2_5.setText("y-offset [m]:")
+        self.label_2_5.move(400,270)
+        self.label_2_5.setFont(QtGui.QFont("Arial",12))
+        self.label_2_5.adjustSize()
+
+        self.textbox_2_5 = QLineEdit(self)
+        self.textbox_2_5.move(550,265)
+        self.textbox_2_5.setFont(QtGui.QFont("times",14))
+        
+
+        self.label_2_6.setText("z-offset [m]:")
+        self.label_2_6.move(400,325)
+        self.label_2_6.setFont(QtGui.QFont("Arial",12))
+        self.label_2_6.adjustSize()
+
+        self.textbox_2_6 = QLineEdit(self)
+        self.textbox_2_6.move(550,320)
+        self.textbox_2_6.setFont(QtGui.QFont("times",14))
+        
+
+        self.label_2_7.setText("Element type:")
+        self.label_2_7.move(400,375)
+        self.label_2_7.setFont(QtGui.QFont("Arial",12))
+        self.label_2_7.adjustSize()
+
+        self.textbox_2_7 = QLineEdit(self)
+        self.textbox_2_7.move(550,370)
+        self.textbox_2_7.setFont(QtGui.QFont("times",14))
+        
+        
+        self.label_2_8.setText("Mesh type:")
+        self.label_2_8.move(400,435)
+        self.label_2_8.setFont(QtGui.QFont("Arial",12))
+        self.label_2_8.adjustSize()
+
+        self.textbox_2_8 = QLineEdit(self)
+        self.textbox_2_8.move(550,430)
+        self.textbox_2_8.setFont(QtGui.QFont("times",14))
+        
+        self.label_2_10.setText("Wing type\n(Planar or Flexibe):")
+        self.label_2_10.move(400,475)
+        self.label_2_10.setFont(QtGui.QFont("Arial",12))
+        self.label_2_10.adjustSize()
+
+        self.textbox_2_10 = QLineEdit(self)
+        self.textbox_2_10.move(550,485)
+        self.textbox_2_10.setFont(QtGui.QFont("times",14))
+        
+
+        self.label_2_11.setText("Number of elements:")
+        self.label_2_11.move(390,535)
+        self.label_2_11.setFont(QtGui.QFont("Arial",12))
+        self.label_2_11.adjustSize()
+
+        self.textbox_2_11 = QLineEdit(self)
+        self.textbox_2_11.move(550,530)
+        self.textbox_2_11.setFont(QtGui.QFont("times",14))
+        
+
+        self.label_2_12.setText("r:")
+        self.label_2_12.move(520,580)
+        self.label_2_12.setFont(QtGui.QFont("Arial",12))
+        self.label_2_12.adjustSize()
+
+        self.textbox_2_12 = QLineEdit(self)
+        self.textbox_2_12.move(550,580)
+        self.textbox_2_12.setFont(QtGui.QFont("times",14))
+        
+     
+
+        self.label_2_13.setText("Uinf [m/s]:")
+        self.label_2_13.move(465,630)
+        self.label_2_13.setFont(QtGui.QFont("Arial", 12))
+
+        self.textbox_2_13 = QLineEdit(self)
+        self.textbox_2_13.move(550,630)
+        self.textbox_2_13.setFont(QtGui.QFont("times",14))
+
+    def on_click(self):
+
+        self.Nwings_1 = int(self.textbox_1_2.text())
+        self.span_1 = float(self.textbox_1_3.text())
+        self.AoA_user_1 = float(self.textbox_1_4.text())
+        self.y_offset_user_1 = float(self.textbox_1_5.text())
+        self.z_offset_user_1 = float(self.textbox_1_6.text())
+        self.elem_type_1 = self.textbox_1_7.text()
+        self.mesh_type_1 = self.textbox_1_8.text()
+        self.wing_type_1 = self.textbox_1_10.text()
+        self.Nelem_1 = int(self.textbox_1_11.text())
+        self.r_1 = float(self.textbox_1_12.text())
+        self.Uinf_1 = float(self.textbox_1_13.text())
+
+        self.Nwings_2 = int(self.textbox_2_2.text())
+        self.span_2 = float(self.textbox_2_3.text())
+        self.AoA_user_2 = float(self.textbox_2_4.text())
+        self.y_offset_user_2 = float(self.textbox_2_5.text())
+        self.z_offset_user_2 = float(self.textbox_2_6.text())
+        self.elem_type_2 = self.textbox_2_7.text()
+        self.mesh_type_2 = self.textbox_2_8.text()
+        self.wing_type_2 = self.textbox_2_10.text()
+        self.Nelem_2 = int(self.textbox_2_11.text())
+        self.r_2 = float(self.textbox_2_12.text())
+        self.Uinf_2 = float(self.textbox_2_13.text())
+
+
+        return #self.Nwings, self.span, self.AoA_user, self.y_offset_user, self.z_offset_user, self.elem_type, self.mesh_type, self.plot_together, self.wing_type, self.Nelem, self.r, self.Uinf
+    
+    def call_Galerkin_def(self):
+        Galerkin_GUI.call_Galerkin(self.Nwings, self.span, self.AoA_user, self.y_offset_user,
+         self.z_offset_user, self.elem_type, self.mesh_type, self.plot_together, self.wing_type, self.Nelem, self.r,self.Uinf)    
+
+    def call_Vortex_sheet(self):
+        Esteira_GUI.call_Vortex_sheet(self.Nelem, self.wing_type,self.Nwings)
+
 
 
 
